be404c3603c074e4c844dadae19ea0e871253ca8
vhhvgdvgfb-source <vhhvgdvgfb@gmail.com>
2025-12-29 22:51:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "26741021")
-    API_HASH = environ.get("API_HASH", "7c5af0b88c33d2f5cce8df5d82eb2a94")
+    API_ID = environ.get("API_ID", "26331872")
+    API_HASH = environ.get("API_HASH", "c93589620441707c37c5683a02eea54e")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vegamoviesforwordbot") 
-    DATABASE_URI = environ.get("DATABASE", "")
+    DATABASE_URI = environ.get("DATABASE", "tnok3d0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6859451629').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002084819782'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002437541681") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '8413546109').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003670474351'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1003670474351") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
